c3d15d597e5bbd90107e4103f6365b7ef5720099
4psireal2 <psireal.harmonics@gmail.com>
2026-06-24 10:16:43 +0200

    comment out ED functions

--- test/helpers/hubbard.jl
+++ test/helpers/hubbard.jl
@@ -1,6 +1,6 @@
 using ITensors
-using SparseArrays
-using KrylovKit
+# using SparseArrays
+# using KrylovKit
 
 
 col_major_site(i::Int, j::Int, Lx::Int) = i + (j - 1) * Lx
@@ -62,238 +62,238 @@ function build_nn_dd_corr_op(Lx::Int, Ly::Int)
 end
 
 ### functions for ED
-function nn_bonds_linear(Lx::Int, Ly::Int)
-    bonds = Tuple{Int,Int}[]
+# function nn_bonds_linear(Lx::Int, Ly::Int)
+#     bonds = Tuple{Int,Int}[]
 
-    for i in 1:Lx, j in 1:Ly
-        a = col_major_site(i, j, Lx)
+#     for i in 1:Lx, j in 1:Ly
+#         a = col_major_site(i, j, Lx)
 
-        if j < Ly
-            push!(bonds, (a, col_major_site(i, j+1, Lx)))
-        end
+#         if j < Ly
+#             push!(bonds, (a, col_major_site(i, j+1, Lx)))
+#         end
 
-        if i < Lx
-            push!(bonds, (a, col_major_site(i+1, j, Lx)))
-        end
-    end
+#         if i < Lx
+#             push!(bonds, (a, col_major_site(i+1, j, Lx)))
+#         end
+#     end
 
-    return bonds
-end
+#     return bonds
+# end
 
-function fixed_N_basis(N::Int, Nf::Int)
-    basis = UInt64[]
+# function fixed_N_basis(N::Int, Nf::Int)
+#     basis = UInt64[]
 
-    for s in UInt64(0):(UInt64(1) << N) - UInt64(1)
-        if count_ones(s) == Nf
-            push!(basis, s)
-        end
-    end
+#     for s in UInt64(0):(UInt64(1) << N) - UInt64(1)
+#         if count_ones(s) == Nf
+#             push!(basis, s)
+#         end
+#     end
 
-    return basis
-end
+#     return basis
+# end
 
-function bitcount_between(state::UInt64, a::Int, b::Int)
-    lo, hi = min(a, b), max(a, b)
+# function bitcount_between(state::UInt64, a::Int, b::Int)
+#     lo, hi = min(a, b), max(a, b)
 
-    n_between = 0
-    for k in lo+1:hi-1
-        n_between += Int((state >> (k - 1)) & UInt64(1))
-    end
+#     n_between = 0
+#     for k in lo+1:hi-1
+#         n_between += Int((state >> (k - 1)) & UInt64(1))
+#     end
 
-    return n_between
-end
+#     return n_between
+# end
 
-function apply_cdag_c(state::UInt64, to::Int, from::Int)
-    occ_to = (state >> (to - 1)) & UInt64(1)
-    occ_from = (state >> (from - 1)) & UInt64(1)
+# function apply_cdag_c(state::UInt64, to::Int, from::Int)
+#     occ_to = (state >> (to - 1)) & UInt64(1)
+#     occ_from = (state >> (from - 1)) & UInt64(1)
 
-    if occ_from == 0 || occ_to == 1
-        return nothing
-    end
+#     if occ_from == 0 || occ_to == 1
+#         return nothing
+#     end
 
-    new_state = state
-    new_state &= ~(UInt64(1) << (from - 1))
-    new_state |=  (UInt64(1) << (to - 1))
+#     new_state = state
+#     new_state &= ~(UInt64(1) << (from - 1))
+#     new_state |=  (UInt64(1) << (to - 1))
 
-    n_between = bitcount_between(state, to, from)
-    sign = iseven(n_between) ? 1.0 : -1.0
+#     n_between = bitcount_between(state, to, from)
+#     sign = iseven(n_between) ? 1.0 : -1.0
 
-    return new_state, sign
-end
+#     return new_state, sign
+# end
 
-function coordination_numbers_linear(Lx::Int, Ly::Int)
-    N = Lx*Ly
-    z = zeros(Int, N)
+# function coordination_numbers_linear(Lx::Int, Ly::Int)
+#     N = Lx*Ly
+#     z = zeros(Int, N)
 
-    for i in 1:Lx, j in 1:Ly
-        a = col_major_site(i, j, Lx)
+#     for i in 1:Lx, j in 1:Ly
+#         a = col_major_site(i, j, Lx)
 
-        z[a] += (j > 1)
-        z[a] += (j < Ly)
-        z[a] += (i > 1)
-        z[a] += (i < Lx)
-    end
+#         z[a] += (j > 1)
+#         z[a] += (j < Ly)
+#         z[a] += (i > 1)
+#         z[a] += (i < Lx)
+#     end
 
-    return z
-end
+#     return z
+# end
 
-function build_spinless_tV_ED(
-    Lx::Int,
-    Ly::Int,
-    t::Real,
-    V::Real;
-    Nf::Int = (Lx*Ly) ÷ 2,
-    shifted::Bool = false,
-)
-    N = Lx*Ly
-    bonds = nn_bonds_linear(Lx,Ly)
-    z = coordination_numbers_linear(Lx, Ly)
+# function build_spinless_tV_ED(
+#     Lx::Int,
+#     Ly::Int,
+#     t::Real,
+#     V::Real;
+#     Nf::Int = (Lx*Ly) ÷ 2,
+#     shifted::Bool = false,
+# )
+#     N = Lx*Ly
+#     bonds = nn_bonds_linear(Lx,Ly)
+#     z = coordination_numbers_linear(Lx, Ly)
 
-    basis = fixed_N_basis(N, Nf)
-    dim = length(basis)
+#     basis = fixed_N_basis(N, Nf)
+#     dim = length(basis)
 
-    state_to_idx = Dict{UInt64, Int}()
-    for (idx, s) in enumerate(basis)
-        state_to_idx[s] = idx
-    end
+#     state_to_idx = Dict{UInt64, Int}()
+#     for (idx, s) in enumerate(basis)
+#         state_to_idx[s] = idx
+#     end
 
-    rows = Int[]
-    cols = Int[]
-    vals = Float64[]
+#     rows = Int[]
+#     cols = Int[]
+#     vals = Float64[]
 
-    for (col, s) in enumerate(basis)
-        # diagonal part
-        Ediag = 0.0
+#     for (col, s) in enumerate(basis)
+#         # diagonal part
+#         Ediag = 0.0
 
-        for (a, b) in bonds
-            na = Int((s >> (a - 1)) & UInt64(1))
-            nb = Int((s >> (b - 1)) & UInt64(1))
-            Ediag += V * na * nb
-        end
+#         for (a, b) in bonds
+#             na = Int((s >> (a - 1)) & UInt64(1))
+#             nb = Int((s >> (b - 1)) & UInt64(1))
+#             Ediag += V * na * nb
+#         end
 
-        if shifted
-            zN = 0.0
-            for a in 1:N
-                na = Int((s >> (a - 1)) & UInt64(1))
-                zN += z[a] * na
-            end
-            Ediag -= (V / 2) * zN
-        end
+#         if shifted
+#             zN = 0.0
+#             for a in 1:N
+#                 na = Int((s >> (a - 1)) & UInt64(1))
+#                 zN += z[a] * na
+#             end
+#             Ediag -= (V / 2) * zN
+#         end
 
-        push!(rows, col)
-        push!(cols, col)
-        push!(vals, Ediag)
+#         push!(rows, col)
+#         push!(cols, col)
+#         push!(vals, Ediag)
 
-        # off-diagonal hopping
-        for (a, b) in bonds
-            res_ab = apply_cdag_c(s, a, b)
-            if res_ab !== nothing
-                snew, sign = res_ab
-                row = state_to_idx[snew]
+#         # off-diagonal hopping
+#         for (a, b) in bonds
+#             res_ab = apply_cdag_c(s, a, b)
+#             if res_ab !== nothing
+#                 snew, sign = res_ab
+#                 row = state_to_idx[snew]
 
-                push!(rows, row)
-                push!(cols, col)
-                push!(vals, -t * sign)
-            end
+#                 push!(rows, row)
+#                 push!(cols, col)
+#                 push!(vals, -t * sign)
+#             end
 
-            res_ba = apply_cdag_c(s, b, a)
-            if res_ba !== nothing
-                snew, sign = res_ba
-                row = state_to_idx[snew]
+#             res_ba = apply_cdag_c(s, b, a)
+#             if res_ba !== nothing
+#                 snew, sign = res_ba
+#                 row = state_to_idx[snew]
 
-                push!(rows, row)
-                push!(cols, col)
-                push!(vals, -t * sign)
-            end
-        end
-    end
+#                 push!(rows, row)
+#                 push!(cols, col)
+#                 push!(vals, -t * sign)
+#             end
+#         end
+#     end
 
-    H = sparse(rows, cols, vals, dim, dim)
+#     H = sparse(rows, cols, vals, dim, dim)
 
-    return H, basis
-end
+#     return H, basis
+# end
 
-function ground_energy_spinless_tV_ED(
-    Lx::Int,
-    Ly::Int,
-    t::Real,
-    V::Real;
-    Nf::Int = (Lx * Ly) ÷ 2,
-    shifted::Bool = false,
-)
-    H, basis = build_spinless_tV_ED(Lx, Ly, t, V; Nf=Nf, shifted=shifted)
+# function ground_energy_spinless_tV_ED(
+#     Lx::Int,
+#     Ly::Int,
+#     t::Real,
+#     V::Real;
+#     Nf::Int = (Lx * Ly) ÷ 2,
+#     shifted::Bool = false,
+# )
+#     H, basis = build_spinless_tV_ED(Lx, Ly, t, V; Nf=Nf, shifted=shifted)
 
-    vals, vecs, info = eigsolve(H, 1, :SR; ishermitian=true)
-    ψ0 = ComplexF64.(vecs[1])
-    ψ0 ./= norm(ψ0)
+#     vals, vecs, info = eigsolve(H, 1, :SR; ishermitian=true)
+#     ψ0 = ComplexF64.(vecs[1])
+#     ψ0 ./= norm(ψ0)
 
-    return real(vals[1]), ψ0, H, basis
-end
+#     return real(vals[1]), ψ0, H, basis
+# end
 
-function diagonal_observables_ED(ψ, basis, Lx::Int, Ly::Int)
-    N = Lx*Ly
-    bonds = nn_bonds_linear(Lx,Ly)
-    Nb = length(bonds)
+# function diagonal_observables_ED(ψ, basis, Lx::Int, Ly::Int)
+#     N = Lx*Ly
+#     bonds = nn_bonds_linear(Lx,Ly)
+#     Nb = length(bonds)
 
-    probs = abs2.(ψ)
-    probs ./= sum(probs)
+#     probs = abs2.(ψ)
+#     probs ./= sum(probs)
 
-    Ntot_mean = 0.0
-    Ntot2_mean = 0.0
+#     Ntot_mean = 0.0
+#     Ntot2_mean = 0.0
 
-    M_mean = 0.0
-    absM_mean = 0.0
-    M2_mean = 0.0
+#     M_mean = 0.0
+#     absM_mean = 0.0
+#     M2_mean = 0.0
 
-    nn_avg_mean = 0.0
+#     nn_avg_mean = 0.0
 
-    for (p, s) in zip(probs, basis)
-        Ntot = 0
-        M = 0.0
+#     for (p, s) in zip(probs, basis)
+#         Ntot = 0
+#         M = 0.0
 
-        for i in 1:Lx, j in 1:Ly
-            a = col_major_site(i, j, Lx)
-            n = Int((s >> (a - 1)) & UInt64(1))
+#         for i in 1:Lx, j in 1:Ly
+#             a = col_major_site(i, j, Lx)
+#             n = Int((s >> (a - 1)) & UInt64(1))
 
-            Ntot += n
-            M += (-1)^(i + j) * n
-        end
+#             Ntot += n
+#             M += (-1)^(i + j) * n
+#         end
 
-        nn_count = 0.0
-        for (a, b) in bonds
-            na = Int((s >> (a - 1)) & UInt64(1))
-            nb = Int((s >> (b - 1)) & UInt64(1))
-            nn_count += na * nb
-        end
+#         nn_count = 0.0
+#         for (a, b) in bonds
+#             na = Int((s >> (a - 1)) & UInt64(1))
+#             nb = Int((s >> (b - 1)) & UInt64(1))
+#             nn_count += na * nb
+#         end
 
-        nn_avg = nn_count / Nb
+#         nn_avg = nn_count / Nb
 
-        Ntot_mean += p * Ntot
-        Ntot2_mean += p * Ntot^2
+#         Ntot_mean += p * Ntot
+#         Ntot2_mean += p * Ntot^2
 
-        M_mean += p * M
-        absM_mean += p * abs(M)
-        M2_mean += p * M^2
+#         M_mean += p * M
+#         absM_mean += p * abs(M)
+#         M2_mean += p * M^2
 
-        nn_avg_mean += p * nn_avg
-    end
+#         nn_avg_mean += p * nn_avg
+#     end
 
-    Ntot_var = Ntot2_mean - Ntot_mean^2
+#     Ntot_var = Ntot2_mean - Ntot_mean^2
 
-    return (
-        Ntot = Ntot_mean,
-        Ntot_var = Ntot_var,
+#     return (
+#         Ntot = Ntot_mean,
+#         Ntot_var = Ntot_var,
 
-        M_cdw = M_mean,
-        abs_M_cdw = absM_mean,
-        M_cdw2 = M2_mean,
+#         M_cdw = M_mean,
+#         abs_M_cdw = absM_mean,
+#         M_cdw2 = M2_mean,
 
-        m_cdw = M_mean / N,
-        abs_m_cdw = absM_mean / N,
-        m_cdw2 = M2_mean / N^2,
+#         m_cdw = M_mean / N,
+#         abs_m_cdw = absM_mean / N,
+#         m_cdw2 = M2_mean / N^2,
 
-        S_pi_pi = M2_mean / N,
+#         S_pi_pi = M2_mean / N,
 
-        nn_avg = nn_avg_mean,
-    )
-end
+#         nn_avg = nn_avg_mean,
+#     )
+# end
